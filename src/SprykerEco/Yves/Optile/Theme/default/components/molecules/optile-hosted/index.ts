3645012059ec80cf55d05e3ec48eeaf433fe55dd
danil.moroz <danil.moroz@spryker.com>
2020-05-04 13:38:21 +0300

    ECO-3507: Integrate Ajax payment

---
+++ src/SprykerEco/Yves/Optile/Theme/default/components/molecules/optile-hosted/index.ts
@@ -0,0 +1,2 @@
+import register from 'ShopUi/app/registry';
+export default register('optile-hosted', () => import(/* webpackMode: "lazy" */'./optile-hosted'));
---
+++ src/SprykerEco/Yves/Optile/Theme/default/components/molecules/optile-hosted/optile-hosted.ts
@@ -0,0 +1,9 @@
+import Component from 'ShopUi/models/component';
+
+export default class OptileHosted extends Component {
+    protected readyCallback(): void {}
+
+    protected init(): void {
+        console.log('init');
+    }
+}
