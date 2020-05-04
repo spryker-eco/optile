b1c0f4d9a436205f763064f5214f5cffa964a5b3
danil.moroz <danil.moroz@spryker.com>
2020-05-04 19:21:12 +0300

    ECO-3507: Integrate Ajax payment

--- src/SprykerEco/Yves/Optile/Theme/default/components/molecules/optile-hosted/index.ts
+++ src/SprykerEco/Yves/Optile/Theme/default/components/molecules/optile-hosted/index.ts
@@ -1,2 +1,3 @@
+import './optile-hosted.scss';
 import register from 'ShopUi/app/registry';
 export default register('optile-hosted', () => import(/* webpackMode: "lazy" */'./optile-hosted'));
--- src/SprykerEco/Yves/Optile/Theme/default/components/molecules/optile-hosted/optile-hosted.ts
+++ src/SprykerEco/Yves/Optile/Theme/default/components/molecules/optile-hosted/optile-hosted.ts
@@ -1,19 +1,35 @@
+declare var $;
 import Component from 'ShopUi/models/component';
-// import ScriptLoader from 'ShopUi/components/molecules/script-loader/script-loader';
-import $ from 'jquery/dist/jquery';
-import '../../../third-party/op-payment-widget/op-payment-widget-v3-3.6.0.min.js';
+import '../../../third-party/op-payment-widget/op-payment-widget-v3.js';
 
 export default class OptileHosted extends Component {
     protected form: HTMLElement;
-    // protected scriptLoader: ScriptLoader;
     
     protected readyCallback(): void {}
 
     protected init(): void {
-        this.form = <HTMLElement>this.getElementsByClassName(`${this.jsName}__form`)[0];
-        // this.scriptLoader = <ScriptLoader>this.getElementsByTagName('script-loader')[0];
+        this.form = <HTMLElement>document.getElementById(`${this.name}`);
 
-        $(this.form).checkoutList();
-        // console.log(this.scriptLoader);
+        this.initPayment();
+    }
+
+    protected initPayment(): void {
+        $(this.form).checkoutList({
+            payButton: this.submitId,
+            baseUrl: this.baseUrl,
+            listId: this.longId
+        });
+    }
+
+    protected get submitId(): string {
+        return this.getAttribute('data-submit-id');
+    }
+
+    protected get longId(): string {
+        return this.getAttribute('data-long-id');
+    }
+
+    protected get baseUrl(): string {
+        return this.getAttribute('data-base-url');
     }
 }
